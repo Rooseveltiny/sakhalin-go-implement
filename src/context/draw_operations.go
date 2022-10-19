593e844da447b3d3b4d344908bc20b33ab16f438
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2022-10-19 15:48:53 +0300

    implemented some methods of context

--- src/context/draw_operations.go
+++ src/context/draw_operations.go
@@ -1,15 +1,37 @@
 package context
 
 import (
-	"maryprotocol/flags"
 	"sakhalin/color"
+	"sakhalin/flags"
 )
 
 func (ctx *Context) FillStyle(col color.IColor) {
-	ctx.AppendBytes(flags.B_FillStyle)
+	ctx.AppendSingleByte(flags.B_FillStyle)
 	ctx.AppendColor(col)
 }
 
 // gradient
 
 // pattern
+
+func (ctx *Context) Font(font string) {
+	ctx.AppendSingleByte(flags.B_Font)
+	ctx.AppendString(font)
+}
+
+func (ctx *Context) GlobalAlpha(alpha float64) {
+	ctx.AppendSingleByte(flags.B_GlobalAlpha)
+	ctx.AppendFloat64(alpha)
+}
+
+func (ctx *Context) GlobalCompositeOperation(mode flags.CompositeOperation) {
+	ctx.AppendSingleByte(flags.B_GlobalCompositeOperation)
+	ctx.AppendSingleByte(byte(mode))
+}
+
+// image smooth
+
+func (ctx *Context) LineCap(cap flags.LineCap) {
+	ctx.AppendSingleByte(flags.B_LineCap)
+	ctx.AppendSingleByte(byte(cap))
+}
