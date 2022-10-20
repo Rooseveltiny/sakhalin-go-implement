f65ee2c91bdc6612f5d5b75d77b8cf6ad048abc3
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2022-10-20 09:47:04 +0300

    added keyboard events

--- src/events/keys_events.go
+++ src/events/keys_events.go
@@ -1,1 +1,18 @@
 package events
+
+import "sakhalin/flags"
+
+type KeyboardEvent struct {
+	Key string
+	Mod flags.ModifierKeys
+}
+
+func (e KeyboardEvent) Bitmask() flags.EventMask { return flags.I_maskKeyDown | flags.I_maskKeyUp }
+
+type KeyDownEvent struct{ KeyboardEvent }
+
+func (e KeyDownEvent) Bitmask() flags.EventMask { return flags.I_maskKeyDown }
+
+type KeyUpEvent struct{ KeyboardEvent }
+
+func (e KeyUpEvent) Bitmask() flags.EventMask { return flags.I_maskKeyUp }
